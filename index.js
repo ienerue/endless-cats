5c2825883d8e2f0e0780b08c2151b26b7dde367d
ienerue <eurenei@proton.me>
2024-10-05 17:45:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 async function getCats() {
     const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=10")
     const cats = await response.json()
-    console.log(cats)
+    cats.forEach(cat => console.log(cat.url))
 }
 
 getCats()
